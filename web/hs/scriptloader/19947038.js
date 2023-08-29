c76c9e083eba5aa0ac9bfefb715c734d53c12ed2
Zoungrana Elkana Daniel <edanzoun@outlook.fr>
2023-08-29 10:17:05 +0000

    first commit

---
+++ web/hs/scriptloader/19947038.js
@@ -0,0 +1,7 @@
+// HubSpot Script Loader. Please do not block this resource. See more: http://hubs.ly/H0702_H0
+
+var _hsp = window._hsp = window._hsp || [];
+ _hsp.push(['addEnabledFeatureGates', []]);
+!function(t,e,r){if(!document.getElementById(t)){var n=document.createElement("script");for(var a in n.src="https://js.hs-banner.com/v2/19947038/banner.js",n.type="text/javascript",n.id=t,r)r.hasOwnProperty(a)&&n.setAttribute(a,r[a]);var i=document.getElementsByTagName("script")[0];i.parentNode.insertBefore(n,i)}}("cookieBanner-19947038",0,{"data-cookieconsent":"ignore","data-hs-ignore":true,"data-loader":"hs-scriptloader","data-hsjs-portal":19947038,"data-hsjs-env":"prod","data-hsjs-hublet":"na1"});
+!function(e,t){if(!document.getElementById(e)){var c=document.createElement("script");c.src="https://js.hs-analytics.net/analytics/1693252200000/19947038.js",c.type="text/javascript",c.id=e;var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(c,n)}}("hs-analytics");
+!function(t,e,r){if(!document.getElementById(t)){var n=document.createElement("script");for(var a in n.src="https://js.hscollectedforms.net/collectedforms.js",n.type="text/javascript",n.id=t,r)r.hasOwnProperty(a)&&n.setAttribute(a,r[a]);var i=document.getElementsByTagName("script")[0];i.parentNode.insertBefore(n,i)}}("CollectedForms-19947038",0,{"crossorigin":"anonymous","data-leadin-portal-id":19947038,"data-leadin-env":"prod","data-loader":"hs-scriptloader","data-hsjs-portal":19947038,"data-hsjs-env":"prod","data-hsjs-hublet":"na1"});
